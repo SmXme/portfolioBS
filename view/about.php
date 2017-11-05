bff129fe1e9fe414cb4c406a626ec324d1d1e58c
SmXme <pillot.clement@gmail.com>
2017-11-05 17:00:36 +0100

    Added some css to improve the sidebar's design in the about page

--- view/about.php
+++ view/about.php
@@ -1,5 +1,5 @@
 <section class="row mainSection">
-	<div class="col-md-3">
+	<div class="aboutSidebar col-md-3">
 		<h3><i class="fa fa-id-card" aria-hidden="true"></i>Informations</h3>
 		<h6><i class="fa fa-briefcase" aria-hidden="true"></i>Développeur WEB</h6>
 		<h6><i class="fa fa-home" aria-hidden="true"></i>Chérisay, FR</h6>
@@ -7,25 +7,25 @@
 		<h6><i class="fa fa-phone" aria-hidden="true"></i>06.14.58.44.88</h6>
 		<h3><i class="fa fa-certificate" aria-hidden="true"></i>Compétences</h3>
 		<h5><i class="fa fa-terminal" aria-hidden="true"></i>Développement WEB</h5>
-		<h6><i class="fa fa-html5" aria-hidden="true"></i>HTML</h6>
+		<h6 class="text-center">HTML</h6>
 		<div class="testPercentContainer">
 			<div class="jsp divQuart">
 				25%
 			</div>
 		</div>
-		<h6><i class="fa fa-css3" aria-hidden="true"></i>CSS</h6>
+		<h6 class="text-center">CSS</h6>
 		<div class="testPercentContainer">
 			<div class="jsp divHalf">
 				50%
 			</div>
 		</div>
-		<h6>Javascript</h6>
+		<h6 class="text-center">Javascript</h6>
 		<div class="testPercentContainer">
 			<div class="jsp divtQuart">
 				75%
 			</div>
 		</div>
-		<h6>PHP</h6>
+		<h6 class="text-center">PHP</h6>
 		<div class="testPercentContainer">
 			<div class="jsp divFull">
 				100%
