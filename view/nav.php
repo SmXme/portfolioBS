1cdf976183930b5d72e3204331325a6ed361f588
SmXme <pillot.clement@gmail.com>
2017-11-09 21:43:01 +0100

    Fixed a bug about the nav, wrong href !

--- view/nav.php
+++ view/nav.php
@@ -8,7 +8,7 @@
 		<li class="nav-item"> 
 
 			<!-- class ="dropdown-toggle" data-toggle="dropdown" -->
-			<a <?php echo $aboutON?> class="nav-link" href="#" role="button" aria-haspopup="true" aria-expanded="false" href="index.php?page=about">QUI SUIS-JE ?</a>
+			<a <?php echo $aboutON?> class="nav-link" href="index.php?page=about" role="button" aria-haspopup="true" aria-expanded="false" href="index.php?page=about">QUI SUIS-JE ?</a>
 <!-- 			<div id ="divDropDown" class="text-center">
 				<a class="dropdown-item" href="index.php?page=about">MON PARCOURS</a>
 				<a class="dropdown-item" href="index.php?page=about">MES COMPÉTENCES</a>
