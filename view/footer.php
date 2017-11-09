523fcff274eb698267550c5070f91b0ec00514d5
SmXme <pillot.clement@gmail.com>
2017-11-09 20:24:09 +0100

    Added a copyright line in the footer

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
 <footer class="row d-flex-row align-items-center bg-myBlue text-center">
 	<div class="p-0 col-lg-4 d-none d-lg-block">
-		<p class="m-0">Designé et Codé par Clément "SmXme" PILLOT</p>
+		<p class="m-0"><i class="fa fa-copyright" aria-hidden="true"></i> Copyright 2017 - Clément "SmXme" PILLOT</p>
 	</div>
 	<div class="p-0 col-sm-6 col-md-6 col-lg-4">
 		<a href="index.php?page=mentions" class="btn btn-sm btn-outline m-0" id="btnLinkMentions" role="button" aria-disabled="true">Mention légales</a>
