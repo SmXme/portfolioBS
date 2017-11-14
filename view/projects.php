b4facba1657b6ed32781c165a340e5fc135815cc
SmXme <pillot.clement@gmail.com>
2017-11-14 18:52:06 +0100

    Added 2 cards in the projects page

--- view/projects.php
+++ view/projects.php
@@ -1,2 +1,19 @@
 <section class="row mainSection">
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="images/screenshotGestemps.png" alt="Screenshot de l'application Gestemps">
+  <div class="card-body">
+    <h4 class="card-title">Gestemps</h4>
+    <p class="card-text">Application de gestion de temps de travail existante lors de mon arrivée pour un stage d'un mois au sein de l'IRFSS Croix Rouge d'Alençon. J'ai participé à une refonte graphique ainsi que la création de certains modules utilisateurs. </p>
+    <a href="#" class="btn btn-primary">Visiter</a>
+  </div>
+</div>
+
+<div class="card" style="width: 20rem;">
+  <img class="card-img-top" src="images/screenshotRankMe.png" alt="Screenshot de l'application RankMeAPP">
+  <div class="card-body">
+    <h4 class="card-title">RankMeAPP</h4>
+    <p class="card-text">Application créée lors d'un stage, sur un thème imposé étant l'affichage et le traitement de donnés, mais un sujet de mon propre choix. J'ai donc utilisé un plugin deathmatch CSGO afin de recueillir les stats joueurs dans un fichier SQL afin de les exploiter.</p>
+    <a href="#" class="btn btn-primary">Visiter</a>
+  </div>
+</div>
 </section>
