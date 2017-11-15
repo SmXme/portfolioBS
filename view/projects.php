44b87f32d3742a4bd64c7eaa3b2480ba4d91740a
SmXme <pillot.clement@gmail.com>
2017-11-15 13:46:49 +0100

    Added new screenshots to use on the cards in the projects page.

--- view/projects.php
+++ view/projects.php
@@ -1,6 +1,6 @@
 <section class="row mainSection d-flex flex-row justify-content-around align-items-center">
 <div class="card">
-  <img class="card-img-top" src="images/screenshotGestemps.png" alt="Screenshot de l'application Gestemps">
+  <img class="card-img-top" src="images/screenshotFullscreen_gestemps-sp4.png" alt="Screenshot de l'application Gestemps">
   <div class="card-body">
     <h4 class="card-title">Gestemps</h4>
     <p class="card-text">Application de gestion de temps de travail existante lors de mon arrivée pour un stage d'un mois au sein de l'IRFSS Croix Rouge d'Alençon. J'ai participé à une refonte graphique ainsi que la création de certains modules utilisateurs. </p>
@@ -9,11 +9,22 @@
 </div>
 
 <div class="card">
-  <img class="card-img-top" src="images/screenshotRankMe.png" alt="Screenshot de l'application RankMeAPP">
+  <img class="card-img-top" src="images/screenshotFullscreen_rankmeapp-sp4.png" alt="Screenshot de l'application RankMeAPP">
   <div class="card-body">
     <h4 class="card-title">RankMeAPP</h4>
     <p class="card-text">Application créée lors d'un stage, sur un thème imposé étant l'affichage et le traitement de donnés, mais un sujet de mon propre choix. J'ai donc utilisé un plugin deathmatch CSGO afin de recueillir les stats joueurs dans un fichier SQL afin de les exploiter.</p>
     <a href="http://rankcsgofr.smxme.fr" target="_blank" class="btn btn-primary">Visiter</a>
   </div>
 </div>
+
+<div class="card">
+  <img class="card-img-top" src="images/screenshotFullscreen_portfoliobs-sp4.png" alt="Screenshot de l'application RankMeAPP">
+  <div class="card-body">
+    <h4 class="card-title">Site personnel</h4>
+    <p class="card-text">Site réalisé en tant que projet personnel dans le cadre de ma formation actuelle de Codeur / Développeur / Intégrateur WEB</p>
+    <a href="https://portfoliobs.smxme.fr" target="_blank" class="btn btn-primary">Visiter</a>
+  </div>
+</div>
+
+
 </section>
