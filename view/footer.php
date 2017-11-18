65d13152867a79907b01ede74b777a462a662a94
SmXme <pillot.clement@gmail.com>
2017-11-18 12:14:02 +0100

    Added a linkedin icon and changed the divRS place in the footer

--- view/footer.php
+++ view/footer.php
@@ -2,12 +2,13 @@
 	<div class="p-0 col-lg-4 d-none d-lg-block">
 		<p class="m-0"><i class="fa fa-copyright" aria-hidden="true"></i> Copyright 2017 - Clément "SmXme" PILLOT</p>
 	</div>
-	<div class="p-0 col-sm-6 col-md-6 col-lg-4">
-		<a href="index.php?page=mentions" class="btn btn-sm btn-outline m-0" id="btnLinkMentions" role="button" aria-disabled="true">Mention légales</a>
-	</div>
 	<div id="divFooterRS" class="p-0 col-sm-6 col-md-6 col-lg-4">
 		<a href="https://github.com/SmXme/" target="_blank"><i class="fa fa-github-square" aria-hidden="true"></i></a>
+		<a href="https://www.linkedin.com/in/cl%C3%A9ment-pillot-8827a6140/" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
 		<a href="https://www.facebook.com/SmXme/" target="_blank"><i class="fa fa-facebook-square ml-1" aria-hidden="true"></i></a>
 		<a href="https://www.twitter.com/_smxme" target="_blank"><i class="fa fa-twitter-square ml-1" aria-hidden="true"></i></a>
+	</div>
+	<div class="p-0 col-sm-6 col-md-6 col-lg-4">
+		<a href="index.php?page=mentions" class="btn btn-sm btn-outline m-0" id="btnLinkMentions" role="button" aria-disabled="true">Mention légales</a>
 	</div>
 </footer>
