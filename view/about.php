570bc3d81390bc7b8a78f37d4cb8927aba7a6e18
SmXme <pillot.clement@gmail.com>
2017-11-21 14:41:20 +0100

    Did some modifications in the about page

--- view/about.php
+++ view/about.php
@@ -4,7 +4,7 @@
 			<img src="images/myPhoto.jpg" alt="Photo de l'auteur du site">
 		</div>
 		<h3><i class="fa fa-id-card" aria-hidden="true"></i>Informations</h3>
-		<h6><i class="fa fa-user-circle" aria-hidden="true"></i>PILLOT Clément (22 ans)</h6>
+		<h6><i class="fa fa-user-circle" aria-hidden="true"></i>Clément PILLOT (22 ans)</h6>
 		<h6><i class="fa fa-briefcase" aria-hidden="true"></i>Développeur WEB</h6>
 		<h6><i class="fa fa-home" aria-hidden="true"></i>Chérisay, FR</h6>
 		<h6><i class="fa fa-envelope" aria-hidden="true"></i>pillot.clement@gmail.com</h6>
@@ -13,14 +13,14 @@
 		<h5 class="subTitle"><i class="fa fa-terminal" aria-hidden="true"></i>Développement WEB</h5>
 		<h6 class="text-center labelGauge">HTML</h6>
 		<div class="testPercentContainer">
-			<div class="jsp divtQuart">
-				<span class="txtPercent">75%</span>
+			<div class="jsp divNinety">
+				<span class="txtPercent">90%</span>
 			</div>
 		</div>
 		<h6 class="text-center labelGauge">CSS</h6>
 		<div class="testPercentContainer">
-			<div class="jsp divtQuart">
-				<span class="txtPercent">75%</span>
+			<div class="jsp divNinety">
+				<span class="txtPercent">90%</span>
 			</div>
 		</div>
 		<h6 class="text-center labelGauge">Javascript</h6>
@@ -37,8 +37,8 @@
 		</div>
 		<h6 class="text-center labelGauge">SQL</h6>
 		<div class="testPercentContainer">
-			<div class="jsp divQuart">
-				<span class="txtPercent">25%<span>
+			<div class="jsp divHalf">
+				<span class="txtPercent">50%<span>
 			</div>
 		</div>
 
@@ -74,19 +74,19 @@
 	<div id="divAbout" class="txtOrange col-md mCustomScrollbar pr-0" data-mcs-theme="inset">
 		<h2 class="mt-3"><i class="fa fa-suitcase" aria-hidden="true"></i>Mes expériences</h2>
 		<ul class="mt-3">
-			<li><p class="m-0"><span class="badge badge-secondary">Mars 2017 / Avril 2017</span> - <b>Derdre-IT</b> - <i>Paris.</i></p>
+			<li><p class="m-0"><span class="badge badge-secondary">Mars 2017 / Avril 2017</span> - <b>Stagiaire Développeur</b> - <b>Derdre-IT</b> - <i>Paris.</i></p>
 				<ul>
 					<li>Création d'une application de traitements et d'affichages de statistiques, provenant d'un fichier SQL généré par un plugin sur un serveur de jeu (CSGO).</li>
 					<li class="list-unstyled"><span class="badge badge-pill badge-language">HTML</span><span class="badge badge-pill badge-language">CSS</span><span class="badge badge-pill badge-language">PHP</span><span class="badge badge-pill badge-language">SQL</span></li>
 				</ul>
 			</li>
-			<li class="mt-3"><p class="m-0"><span class="badge badge-secondary">Février 2016 / Mars 2016</span> - <b>ASPerience</b> - <i>Damigny.</i></p>
+			<li class="mt-3"><p class="m-0"><span class="badge badge-secondary">Février 2016 / Mars 2016</span> - <b>Stagiaire Développeur</b> - <b>ASPerience</b> - <i>Damigny.</i></p>
 				<ul>
 					<li>Débuggage et adaptation d'eGroupware pour une version plus récente de PHP, afin de faire fonctionner la messagerie avec les versions d'outlook plus récentes.</li>
 					<li class="list-unstyled"><span class="badge badge-pill badge-language">HTML</span><span class="badge badge-pill badge-language">CSS</span><span class="badge badge-pill badge-language">PHP</span><span class="badge badge-pill badge-language">SQL</span></li>
 				</ul>
 			</li>
-			<li class="mt-3"><p class="m-0"><span class="badge badge-secondary">Avril 2015 / Mai 2015</span> - <b>IRFSS Croix-Rouge</b> - <i>Alençon.</i></p>
+			<li class="mt-3"><p class="m-0"><span class="badge badge-secondary">Avril 2015 / Mai 2015</span> - <b>Stagiaire Développeur</b> - <b>IRFSS Croix-Rouge</b> - <i>Alençon.</i></p>
 				<ul>
 					<li>Refonte graphique, amélioration, et création de modules pour une application régionale de gestion du temps de travail des employés.</li>
 					<li class="list-unstyled"><span class="badge badge-pill badge-language">HTML</span><span class="badge badge-pill badge-language">CSS</span><span class="badge badge-pill badge-language">PHP</span><span class="badge badge-pill badge-language">SQL</span></li>
@@ -95,7 +95,7 @@
 		</ul>
 		<h2 class="mt-3"><i class="fa fa-graduation-cap" aria-hidden="true"></i>Mon cursus</h2>
 		<ul class="mt-3 mb-0">
-			<li><p><span class="badge badge-secondary">En Cours</span> - <b>Formation Codeur Développeur Intégrateur WEB</b> - <i>ISF, Damigny.</i></p></li>
+			<li><p><span class="badge badge-secondary">En Cours</span> - <b>Certification WebForce3 Codeur Développeur Intégrateur WEB</b> - <i>ISF, Damigny.</i></p></li>
 			<li><p><span class="badge badge-secondary">2013 / 2017 </span> - <b>BTS Service Informatique aux Organisations option Solutions Logicielles et Applications Métiers</b> - <i>CNED, Poitiers.</i></p></li>
 			<li><p><span class="badge badge-secondary">2012 / 2013 </span> - <b>DUT Informatique 1ère Année</b> - <i>IUT, Maubeuge.</i></p></li>
 			<li><p><span class="badge badge-secondary">2010 / 2012 </span> - <b>BAC Sciences et Technologies Industrielles option Structure Métallique</b> - <i>Lycée Pierre Forest, Maubeuge.</i></p></li>
