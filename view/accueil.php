b56853cac0e08124c736774d77f06bab0eda3598
SmXme <pillot.clement@gmail.com>
2017-12-01 18:03:16 +0100

    Corrected a missing character in the home page

--- view/accueil.php
+++ view/accueil.php
@@ -7,7 +7,7 @@
 	<article id="homeArticle" class="col pl-0 d-flex align-items-center txtOrange text-center">
 		<div class="container d-flex flex-column align-items-center mr-xl-5">
 			<h2 class="pb-5">Bienvenue sur mon site personnel.</h2>
-			<p class="text-justify">Je suis Clément "SmXme" PILLOT, né le 31 Décembre 1994, et passioné d'informatique ainsi que de jeux vidéos depuis mon plus jeune âge.</p>
+			<p class="text-justify">Je suis Clément "SmXme" PILLOT, né le 31 Décembre 1994, et passionné d'informatique ainsi que de jeux vidéos depuis mon plus jeune âge.</p>
 			<p class="text-justify">De la construction à l'installation d'une machine, jusqu'au développement, j'ai choisi de miser sur la polyvalence. Evoluant dans l'expertise du développement WEB, dans le but de devenir Full Stack.</p>
 			<a href="index.php?page=about" class="btn btn-primary btn-lg mt-5" id="btnLinkHome" role="button" aria-disabled="true">En savoir plus</a>
 		</div>
