8e5dc88881e37737869a7abee9f934fac229552c
Unknown <pillot.clement@gmail.com>
2018-09-26 08:52:25 +0200

    Added Sendwish's card and corrected the gestemps URL

--- view/projects.php
+++ view/projects.php
@@ -1,18 +1,18 @@
 <div class="container">
 <section class="row mainSection d-flex flex-row justify-content-around align-items-center">
-  <div class="col-xs-12 mt-4 mt-md-0 col-md-4 d-flex justify-content-center">
+  <div class="col-xs-12 mt-4 mt-md-0 col-md-3 d-flex justify-content-center">
     <div class="card">
        <a href="http://gestemps.smxme.fr/gestemps.php" target="_blank"><img class="card-img-top" src="images/screenshotFullscreen_gestemps-sp4.png" alt="Screenshot de l'application Gestemps"></a>
       <div class="card-body">
         <h4 class="card-title">Gestemps</h4>
         <p class="card-text">Application de gestion de temps de travail.</p> 
         <p class="card-text">J'ai réalisé une refonte graphique ainsi que la création de certains modules utilisateurs, lors d'un stage d'un mois au sein de L'IRFSS Croix Rouge d'Alençon.</p>
-        <a href="http://gestemps.smxme.fr/gestemps.php" target="_blank" class="btn btn-primary">Visiter</a>
+        <a href="http://gestemps.smxme.fr/Gestemps.php" target="_blank" class="btn btn-primary">Visiter</a>
       </div>
     </div>
   </div>
 
-  <div class="col-xs-12 mt-4 mt-md-0 col-md-4 d-flex justify-content-center">
+  <div class="col-xs-12 mt-4 mt-md-0 col-md-3 d-flex justify-content-center">
     <div class="card">
       <a href="http://rankcsgofr.smxme.fr" target="_blank"><img class="card-img-top" src="images/screenshotFullscreen_rankmeapp-sp4.png" alt="Screenshot de l'application RankMeAPP"></a>
       <div class="card-body">
@@ -23,13 +23,24 @@
       </div>
     </div>
 </div>
-<div class="col-xs-12 mt-4 mt-md-0 mb-4 mb-md-0 col-md-4 d-flex justify-content-center">
+<div class="col-xs-12 mt-4 mt-md-0 mb-4 mb-md-0 col-md-3 d-flex justify-content-center">
     <div class="card">
        <a href="https://smxme.fr" target="_blank"><img class="card-img-top" src="images/screenshotFullscreen_portfoliobs-sp4.png" alt="Screenshot de l'application RankMeAPP"></a>
       <div class="card-body">
         <h4 class="card-title">Site personnel</h4>
-        <p class="card-text">Site réalisé en tant que projet personnel dans le cadre de ma formation actuelle de Codeur / Développeur / Intégrateur WEB.</p>
+        <p class="card-text">Portfolio réalisé en tant que projet personnel au début de ma formation de Codeur / Développeur / Intégrateur WEB.</p>
         <a href="https://smxme.fr" target="_blank" class="btn btn-primary">Visiter</a>
+      </div>
+    </div>
+  </div>
+
+<div class="col-xs-12 mt-4 mt-md-0 mb-4 mb-md-0 col-md-3 d-flex justify-content-center">
+    <div class="card">
+       <a href="https://smxme.fr" target="_blank"><img class="card-img-top" src="images/screenshotFullscreen_sendwish-sp4.png" alt="Screenshot de l'application RankMeAPP"></a>
+      <div class="card-body">
+        <h4 class="card-title">Sendwish</h4>
+        <p class="card-text">Site d'un fast-food fictif réalisé en tant que projet de groupe de fin de formation, faisant état de nos acquis et à présenter devant un jury.</p>
+        <a href="https://sendwish.smxme.fr" target="_blank" class="btn btn-primary">Visiter</a>
       </div>
     </div>
   </div>
