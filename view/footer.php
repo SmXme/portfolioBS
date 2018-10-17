953034ac0da8b43c90ff132449c2ef0bc9412209
Unknown <pillot.clement@gmail.com>
2018-10-17 16:17:28 +0200

    Modif Footer

--- view/footer.php
+++ view/footer.php
@@ -1,11 +1,11 @@
 <footer class="row d-flex-row align-items-center bg-myBlue text-center">
 	<div class="p-0 col-lg-4 d-none d-lg-block">
-		<p class="m-0"><i class="fa fa-copyright" aria-hidden="true"></i> Copyright 2017 - Clément "SmXme" PILLOT</p>
+		<p class="m-0"><i class="fa fa-copyright" aria-hidden="true"></i> Copyright 2018 - Clément "SmXme" PILLOT</p>
 	</div>
 	<div id="divFooterRS" class="p-0 col-sm-6 col-md-6 col-lg-4">
 		<a href="https://github.com/SmXme/" target="_blank"><i class="fab fa-github-square ml-1" aria-hidden="true"></i></a>
 
-		<a href="www.linkedin.com/in/SmXme" target="_blank"><i class="fab fa-linkedin ml-1" aria-hidden="true"></i></a>
+		<a href="https://www.linkedin.com/in/SmXme" target="_blank"><i class="fab fa-linkedin ml-1" aria-hidden="true"></i></a>
 		<a href="https://www.facebook.com/SmXme/" target="_blank"><i class="fab fa-facebook-square ml-1" aria-hidden="true"></i></a>
 		<a href="https://www.twitter.com/_smxme" target="_blank"><i class="fab fa-twitter-square ml-1" aria-hidden="true"></i></a>
 	</div>
